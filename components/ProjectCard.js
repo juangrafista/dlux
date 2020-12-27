9a874085e3c7525532e9b6310ec0d58952a9bc0c
juangrafista <juangrafista@gmail.com>
2020-12-27 18:45:41 -0500

    fixing images issues with sanity

---
+++ components/ProjectCard.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import styled from 'styled-components'
+import { urlFor } from '../lib/sanity'
+
+const ProjectCard = ({ post }) => {
+  return (
+    <StyledCard>
+      <img
+        src={urlFor(post.mainImage).width(400).format('webp').url()}
+        alt={post.title}
+      />
+      <div className='text'>
+        <h3>{post.title}</h3>
+        <p>{post.excerpt}</p>
+      </div>
+    </StyledCard>
+  )
+}
+
+const StyledCard = styled.div`
+  /* background: white; */
+  border-radius: 10px;
+  overflow: hidden;
+  color: black;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+  transition: transform 0.2s ease;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.02);
+  }
+  .text {
+    padding: 1.2rem;
+    padding-top: 0;
+    margin-top: 0;
+    background-color: white;
+    h3 {
+      padding-bottom: 1rem;
+      padding-top: 1rem;
+    }
+    p {
+      padding-bottom: 1rem;
+    }
+  }
+  img {
+    width: 100%;
+    height: 15rem;
+    object-fit: cover;
+  }
+  @media (max-width: 800px) {
+    .text {
+      padding: 0.5rem;
+      h3 {
+        padding-bottom: 0.5rem;
+        padding-top: 0.5rem;
+      }
+      p {
+        padding-bottom: 0.5rem;
+      }
+    }
+    img {
+      height: 8rem;
+    }
+  }
+`
+
+export default ProjectCard
--- pages/projects.js
+++ pages/projects.js
@@ -2,17 +2,13 @@ import { useState, useEffect } from 'react'
 import Head from 'next/head'
 // components
 import Layout from '../components/Layout'
+import ProjectCard from '../components/ProjectCard'
 // sanity
-import { getAllPosts, getAllPostsForHome } from '../lib/api'
-import { urlFor } from '../lib/sanity'
+import { getAllPosts } from '../lib/api'
+
+import styled from 'styled-components'
 
 export default function Projects({ posts }) {
-  const [postList, setPostList] = useState([])
-
-  useEffect(() => {
-    setPostList(posts)
-  }, [posts])
-
   return (
     <>
       <Head>
@@ -20,20 +16,27 @@ export default function Projects({ posts }) {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Layout>
-        {posts.map((post) => (
-          <div key={post.slug.current}>
-            <h2>{post.title}</h2>
-            <img
-              className='cover'
-              alt={`${post.title}`}
-              src={urlFor(post.mainImage).width(300).format('webp').url()}
-            />
-          </div>
-        ))}
+        <Grid>
+          {posts.map((post) => (
+            <ProjectCard key={post.slug} post={post} />
+          ))}
+        </Grid>
       </Layout>
     </>
   )
 }
+
+const Grid = styled.div`
+  padding: 2rem 10%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 2rem;
+  align-items: center;
+  justify-content: space-between;
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+  }
+`
 
 export async function getStaticProps() {
   const posts = await getAllPosts()
