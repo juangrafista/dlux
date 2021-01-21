7d1189a794f32ada1dc338c31f6afd7ee461726c
juangrafista <juangrafista@gmail.com>
2021-01-21 18:35:31 -0500

    added admin redirect

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ['cdn.sanity.io'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: 'http://dlux.sanity.studio',
+        permanent: true,
+      },
+    ]
+  },
 }
